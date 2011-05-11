2c7541f7ad369c38200065b1a8d92db25a1ee301
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2011-05-11 14:17:51 +0200

    unused
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/main/java/app/controllers/FooBarController.java
+++ src/main/java/app/controllers/FooBarController.java
@@ -10,7 +10,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 import org.bson.types.ObjectId;
-import util.ClassToFields;
 
 import java.util.HashMap;
 import java.util.Map;
